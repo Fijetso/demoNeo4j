ee7f5537c19c3468e7f21c611ef6d7832a1a4dc8
Fijetso <henrynguyen02081997@gmail.com>
2019-10-29 07:52:23 +0700

    code but didn't work

--- src/main/java/com/example/demoNeo4j/DemoNeo4jApplication.java
+++ src/main/java/com/example/demoNeo4j/DemoNeo4jApplication.java
@@ -2,8 +2,16 @@ package com.example.demoNeo4j;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.data.neo4j.Neo4jDataAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
 @SpringBootApplication
+@Configuration
+@ComponentScan(value = { "com.example.demoNeo4j" })
+@EnableAutoConfiguration(exclude = { Neo4jDataAutoConfiguration.class, DataSourceAutoConfiguration.class })
 public class DemoNeo4jApplication {
 
 	public static void main(String[] args) {
